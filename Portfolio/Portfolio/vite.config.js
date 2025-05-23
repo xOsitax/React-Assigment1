22285742d13957e3cdfce46dd4f7016f7a6a4aa1
YesseniaC <98070263+xOsitax@users.noreply.github.com>
2025-05-23 13:37:42 +0200

    e

---
+++ Portfolio/Portfolio/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// Replace this with your actual GitHub repo name
+const repoName = "your-repo-name";
+
+export default defineConfig({
+  base: "/React-Assigment1/",
+  plugins: [react(), tailwindcss()],
+});
