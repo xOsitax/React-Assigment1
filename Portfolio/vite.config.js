a5f7fe6f7493990b691258c0d73dc0dd783cb80a
YesseniaC <98070263+xOsitax@users.noreply.github.com>
2025-05-23 12:42:02 +0200

    deploy

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// Replace this with your actual GitHub repo name
+const repoName = "your-repo-name";
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react(), tailwindcss()],
 });
